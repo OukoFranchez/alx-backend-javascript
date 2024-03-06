bcb7019f54f250ddb937cc56fb7dd89672a542c5
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2024-03-07 02:33:56 +0300

    make eslint compliant

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,9 @@
-const { uploadPhoto, createUser } = require('./utils');
+import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignup() {
-  const promises = [uploadPhoto(), createUser()];
-
-  const results = Promise.all(promises);
-  const resArr = [];
-
-  results
-    .then((result) => result.forEach((res) => {
-      if (res.body) resArr.push(res.body);
-      if (res.firstName) resArr.push(res.firstName);
-      if (res.lastName) resArr.push(res.lastName);
-    })).then(() => console.log(resArr.join(' ')));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log("Signup system offline"));
 }
