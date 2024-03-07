6e16b9b495c40cd9a56d8fa364d9805718f71a68
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2024-03-07 14:17:53 +0300

    add solution to task 2

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,12 @@
+import ClassRoom from './0-classroom';
+
+const initializeRooms = () => {
+  const rooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+  return rooms;
+};
+
+export default initializeRooms;
