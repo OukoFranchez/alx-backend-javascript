c85f4cbbcd95aec5c2d7866fd389a77664f74545
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2024-03-07 18:22:10 +0300

    add solution to task 12

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,23 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  // Range Attribute setter and getter
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
