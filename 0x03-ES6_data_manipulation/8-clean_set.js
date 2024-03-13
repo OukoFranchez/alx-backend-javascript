3154aa31b1428bf22c87704d1322b819a4d6ed3d
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2024-03-13 03:19:21 +0300

    refactor task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,32 @@
-const CleanSet = (set, startString) => {
-  // filter the set to keep only values that start with the specified string
-  const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  // Join the filtered values into a single string separated by '-'
-  return filteredValues.join('-').replace(new RegExp('^${startString'), '');
+const cleanSet = (set, startString) => {
+  // Get the length of the startString
+  const len = startString.length;
+
+  // If startString is empty or undefined, return an empty string
+  if (len <= 0) {
+    return '';
+  }
+
+  // Array to store strings that start with startString
+  const strs = [];
+
+  // Loop through each value in the set
+  for (const str of set.values()) {
+    // Check if the value starts with startString
+    if (str.startsWith(startString)) {
+      // If yes, push the substring after startString to strs array
+      strs.push(str.slice(len));
+    }
+  }
+
+  // If there are strings in strs array, join them with '-' separator
+  if (strs.length > 0) {
+    const finStr = strs.join('-');
+    return finStr;
+  }
+
+  // If strs array is empty, return an empty string
+  return '';
 };
-export default CleanSet;
+
+export default cleanSet;
