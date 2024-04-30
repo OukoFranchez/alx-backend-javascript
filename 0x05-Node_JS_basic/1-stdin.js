d53c2a301d380625d99de0dd78d3ff9ed728e192
OukoFranchez <oukofranchez@gmail.com>
2024-04-30 17:59:32 +0300

    refactor task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,13 @@
-process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
-  process.exit();
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
-process.on('exit', () => {
+
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
